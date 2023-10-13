7967028bcde9d263d27c8352f57cffad6c4f1619
tyri0n11 <chauthinh00710@gmail.com>
2023-10-13 21:53:43 +0700

    Add frame

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame frame = new JFrame();
+        frame.add(new Panel());
+        frame.setTitle("Plant Vs Zombie ");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setVisible(true);
+        frame.setBounds(127, 0, 1024, 625);
     }
 }
---
+++ src/Panel.java
@@ -0,0 +1,14 @@
+import javax.swing.JPanel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Panel extends JPanel implements ActionListener {
+    private final long serialVersionUID = 1L;
+
+    //set Size for Peashooter, Sunflower, Repeater
+    //Size is referenced to Adihendro_PlantsVsZombies_GitHub
+    private int pwidth =62, pheight=66, swidth=pwidth, sheight=pheight+5, rwidth=pwidth+2, rheight=pheight+2;
+    public void actionPerformed(ActionEvent e){
+
+    }
+}
